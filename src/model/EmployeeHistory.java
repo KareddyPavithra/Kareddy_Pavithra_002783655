ae929299257bfc76bfb4232cac5fe7f3c89683a8
KareddyPavithra <kareddy.p@northeastern.edu>
2022-09-30 22:35:12 -0400

    completed create ui

---
+++ src/model/EmployeeHistory.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pavit
+ */
+public class EmployeeHistory {
+    private ArrayList<Employee>history;
+    
+    public EmployeeHistory(){
+        this.history = new ArrayList<Employee>();
+    }
+
+    public ArrayList<Employee> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Employee> history) {
+        this.history = history;
+    }
+    
+    public Employee addNewEmploy(){
+            Employee newEmploy = new Employee();
+            history.add(newEmploy);
+            return newEmploy; 
+    }
+}
